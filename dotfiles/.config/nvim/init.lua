9df56d8b32d6850912050bb3e52988368be0416d
Aaron Baldwin <baldwina@brightwayslearning.org>
2025-11-15 08:31:24 -0700

    switch browser reload to chrome

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -10,8 +10,8 @@ vim.api.nvim_create_user_command("ReloadChrome", function()
   vim.fn.jobstart({
     "osascript",
     "-e",
-    -- 'tell application "Google Chrome" to tell the active tab of its first window to reload',
-    'tell application "Safari" to set URL of document 1 to URL of document 1',
+    'tell application "Google Chrome" to tell the active tab of its first window to reload',
+    -- 'tell application "Safari" to set URL of document 1 to URL of document 1',
   }, { detach = true })
 end, {})
 
